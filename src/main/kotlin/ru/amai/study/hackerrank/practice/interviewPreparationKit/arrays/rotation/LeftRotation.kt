fefe90978523c4eaa88b579fde91b2969d09ff81
Sergey Lobanov <s.y.lobanov@yandex.ru>
2019-08-10 15:36:01 +0300

    add leftRotation

---
+++ src/main/kotlin/ru/amai/study/hackerrank/practice/interviewPreparationKit/arrays/rotation/LeftRotation.kt
@@ -0,0 +1,23 @@
+package ru.amai.study.hackerrank.practice.interviewPreparationKit.arrays.rotation
+
+import java.util.*
+
+fun rotLeft(arr: Array<Int>, d: Int): Array<Int> {
+    val size = arr.size
+    fun shiftedIndex(indx: Int) = ((indx - d) % size + size) % size
+    val newIndexedElems = arr
+        .mapIndexed { indx, elem -> shiftedIndex(indx) to elem }
+        .toMap()
+
+    return (0 until size).map(newIndexedElems::getValue).toTypedArray()
+}
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    val nd = scanner.nextLine().split(" ")
+    val d = nd[1].trim().toInt()
+    val arr = scanner.nextLine().split(" ").map{ it.trim().toInt() }.toTypedArray()
+
+    val result = rotLeft(arr, d)
+    println(result.joinToString(" "))
+}
---
+++ src/test/kotlin/ru/amai/study/hackerrank/practice/interviewPreparationKit/arrays/rotation/LeftRotationKtTest.kt
@@ -0,0 +1,45 @@
+package ru.amai.study.hackerrank.practice.interviewPreparationKit.arrays.rotation
+
+import io.mockk.verify
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments.of
+import org.junit.jupiter.params.provider.MethodSource
+import ru.amai.study.hackerrank.practice.interviewPreparationKit.moduleTest
+import java.util.Arrays.toString
+
+internal class LeftRotationKtTest {
+
+    @ParameterizedTest
+    @MethodSource("arrProvider")
+    fun rotLeftTest(arr: Array<Int>, d: Int, expectedArr: Array<Int>) {
+        assertThat(rotLeft(arr, d))
+            .`as`("state of the %s after performing %s left rotations", toString(arr), d)
+            .isEqualTo(expectedArr)
+    }
+
+    companion object {
+        @Suppress("unused")
+        @JvmStatic
+        fun arrProvider() = listOf(
+            of(arrayOf(1, 2, 3, 4, 5), 4, arrayOf(5, 1, 2, 3, 4)),
+            of(
+                arrayOf(41, 73, 89, 7, 10, 1, 59, 58, 84, 77, 77, 97, 58, 1, 86, 58, 26, 10, 86, 51),
+                10,
+                arrayOf(77, 97, 58, 1, 86, 58, 26, 10, 86, 51, 41, 73, 89, 7, 10, 1, 59, 58, 84, 77)
+            ),
+            of(arrayOf(1, 2, 3), 4, arrayOf(2, 3, 1))
+        )
+    }
+
+    @Test
+    fun leftRotationKtModuleTest() {
+        moduleTest("""
+            5 4
+            1 2 3 4 5
+        """.trimIndent(), ::main) {
+            verify { print("5 1 2 3 4") }
+        }
+    }
+}
