f28573bed510ee33546a2eed580047c744292935
Sergey Lobanov <s.y.lobanov@yandex.ru>
2019-08-27 21:39:50 +0300

    add countTriplets

---
+++ src/main/kotlin/ru/amai/study/hackerrank/practice/interviewPreparationKit/hashmaps/triplets/CountTriplets.kt
@@ -0,0 +1,98 @@
+package ru.amai.study.hackerrank.practice.interviewPreparationKit.hashmaps.triplets
+
+import java.util.*
+
+fun countTriplets(array: LongArray, ratio: Long): Long {
+    fun modR(iv: IndexedValue<Long>) = iv.value % ratio == 0L
+    fun divR(iv: IndexedValue<Long>) = IndexedValue(iv.index, iv.value / ratio)
+
+    fun List<IndexedValue<Long>>.indexes() = map { (index, _) -> index }
+
+    fun candidates(power: Int) =
+        (0 until power).fold(array.withIndex()) { curArr, _ ->
+            curArr.filter(::modR).map(::divR)
+        }.groupBy { (_, value) -> value }
+         .mapValues { (_, indexedValues) -> indexedValues.indexes() }
+
+    val (iCandidates, jCandidates, kCandidates) =
+        listOf(0, 1, 2).map(::candidates)
+
+    val commonBases = kCandidates.keys.filter { v ->
+        (v in iCandidates) && (v in jCandidates)
+    }
+
+    return commonBases.map { v ->
+        val kvCandidates = kCandidates.getValue(v)
+        val jvCandidates = jCandidates.getValue(v)
+        val ivCandidates = iCandidates.getValue(v)
+
+        countTriplesForSingleBaseValue(ivCandidates, jvCandidates, kvCandidates)
+    }.sum()
+}
+
+private fun countTriplesForSingleBaseValue(
+    ivCandidates: List<Int>,
+    jvCandidates: List<Int>,
+    kvCandidates: List<Int>
+): Long {
+    var i = 0
+    var j = 0
+    var k = 0
+    var iCnt = 0L
+    var jCnt = 0L
+    var kCnt = 0L
+
+    while ((k < kvCandidates.size) && (i < ivCandidates.size) && (j < jvCandidates.size)) {
+        val ivc = ivCandidates[i]
+        val jvc = jvCandidates[j]
+        val kvc = kvCandidates[k]
+
+        when {
+            kvc <= jvc -> {
+                kCnt += jCnt
+                k++
+            }
+            jvc <= ivc -> {
+                jCnt += iCnt
+                j++
+            }
+            else -> {
+                i++
+                iCnt += 1L
+            }
+        }
+    }
+
+    while ((k < kvCandidates.size) && (j < jvCandidates.size)) {
+        val jvc = jvCandidates[j]
+        val kvc = kvCandidates[k]
+
+        when {
+            kvc <= jvc -> {
+                kCnt += jCnt
+                k++
+            }
+            else -> {
+                jCnt += iCnt
+                j++
+            }
+        }
+    }
+
+    if ((k < kvCandidates.size) && (kvCandidates[k] > jvCandidates.last())) {
+        kCnt += jCnt * (kvCandidates.size - k)
+    }
+
+    return kCnt
+}
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    val nr = scanner.nextLine().trim().split(" ")
+
+    val r = nr[1].toLong()
+    val array = scanner.nextLine().trim().split(" ").map{ it.toLong() }.toLongArray()
+
+    val result = countTriplets(array, r)
+    println(result)
+}
---
+++ src/test/kotlin/ru/amai/study/hackerrank/practice/interviewPreparationKit/hashmaps/triplets/CountTripletsKtTest.kt
@@ -0,0 +1,46 @@
+package ru.amai.study.hackerrank.practice.interviewPreparationKit.hashmaps.triplets
+
+import io.mockk.verify
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments.of
+import org.junit.jupiter.params.provider.MethodSource
+import ru.amai.study.hackerrank.practice.interviewPreparationKit.moduleTest
+import java.util.*
+
+internal class CountTripletsKtTest {
+
+    @ParameterizedTest
+    @MethodSource("arrayAndRatioProvider")
+    fun countTripletsTest(array: LongArray, ratio: Long, result: Long) {
+        assertThat(countTriplets(array, ratio))
+            .`as`(
+                "count of triplets that form a geometric progression with ratio '%s' in '%s'",
+                ratio,
+                Arrays.toString(array)
+            ).isEqualTo(result)
+    }
+
+    companion object {
+        @Suppress("unused")
+        @JvmStatic
+        fun arrayAndRatioProvider() = listOf(
+            of(longArrayOf(1L, 3L, 9L, 9L, 27L, 81L), 3L, 6L),
+            of(longArrayOf(1L, 5L, 5L, 25L, 125L), 5L, 4L),
+            of(longArrayOf(1L, 2L, 2L, 4L), 2L, 2L),
+            of(longArrayOf(1L, 2L, 4L, 2L, 4L), 2L, 3L),
+            of(longArrayOf(1L, 1L, 1L, 1L, 1L, 1L, 1L), 1L, 35L)
+        )
+    }
+
+    @Test
+    fun countTripletsModuleTest() {
+        moduleTest("""
+                6 3
+                1 3 9 9 27 81
+        """.trimIndent(),
+            ::main) { verify { println(6L) }
+        }
+    }
+}
